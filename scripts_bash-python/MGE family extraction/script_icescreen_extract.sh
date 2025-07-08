4d295052bb36eac935a5e737e5574901a34b25c8
Acapjim <adria.cape@gmail.com>
2025-07-08 16:37:55 +0200

    Delete scripts_bash-python directory

--- scripts_bash-python/MGE family extraction/script_icescreen_extract.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-directorio=/home/adria_tfg/tfg/data/Mobile_elements/icescreen_results/icescreen_adapted_tsv/
-
-for file in "$directorio"/*.tsv
-do
-python3 /home/adria_tfg/tfg/scripts/script_icescreen_extract.py "$file"
-done
--- scripts_bash-python/chromosome finder/script_fasta_chromosome_finder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-directorio=/home/adria_tfg/tfg/data/genomas_sin_duplicaciones
-
-for file in "$directorio"/*.fna
-do
-python3 /home/adria_tfg/tfg/scripts/script_fasta_chromosome_finder.py "$file"
-done
